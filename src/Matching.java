da976714e0a91a4795c93fbbbc09ad3229924d06
DESKTOP-V5MFJTC\林正紘 <masa199606@gmail.com>
2018-10-15 13:10:47 +0900

    ひとまず動くもの

--- src/Matching.java
+++ src/Matching.java
@@ -3,7 +3,7 @@
 
 
 
-  変数:前に？をつける．  
+  変数:前に？をつける．
 
   Examle:
   % Matching "Takayuki" "Takayuki"
@@ -27,97 +27,132 @@
   Unify は，ユニフィケーション照合アルゴリズムを実現し，
   パターン表現を比較して矛盾のない代入によって同一と判断
   できるかどうかを調べる．
-  
+
   ***/
 
-import java.lang.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 /**
  * マッチングのプログラム
- * 
+ *
  */
 class Matching {
     public static void main(String arg[]){
-	if(arg.length != 2){
-	    System.out.println("Usgae : % Matching [string1] [string2]");
-	}
-	System.out.println((new Matcher()).matching(arg[0],arg[1]));
+
+    	if(arg.length == 0) {
+    		System.out.println("Usgae : % Matching [string1] [string2] ...");
+    	}
+
+    	try {    // ファイル読み込みに失敗した時の例外処理のためのtry-catch構文
+            String fileName = "dataset_example.txt"; // ファイル名指定
+
+            // 文字コードUTF-8を指定してBufferedReaderオブジェクトを作る
+            BufferedReader in;
+            for(int i = 0; i < arg.length ; i++) {
+	            // 変数lineに1行ずつ読み込むfor文
+            	System.out.println("`" + arg[i] + "` とマッチングするデータ");
+            	in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
+	            for (String line = in.readLine(); line != null; line = in.readLine()) {
+	            	if(new Matcher().matching(arg[i],line)) {
+	            		System.out.println(line);
+	            	}	
+	            }
+	            System.out.println();
+	            in.close();  // ファイルを閉じる
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace(); // 例外が発生した所までのスタックトレースを表示
+        }
+
     }
 }
 
 /**
  * 実際にマッチングを行うクラス
- * 
+ *
  */
 class Matcher {
     StringTokenizer st1;
     StringTokenizer st2;
     HashMap<String,String> vars;
-    
+
     Matcher(){
-	vars = new HashMap<String,String>();
+    	vars = new HashMap<String,String>();
     }
 
     public boolean matching(String string1,String string2,HashMap<String,String> bindings){
-	this.vars = bindings;
-	if(matching(string1,string2)){
-	    return true;
-	} else {
-	    return false;
-	}
+		this.vars = bindings;
+		if(matching(string1,string2)){
+		    return true;
+		} else {
+		    return false;
+		}
     }
-    
+
     public boolean matching(String string1,String string2){
-	//System.out.println(string1);
-	//System.out.println(string2);
-	
-	// 同じなら成功
-	if(string1.equals(string2)) return true;
-	
-	// 各々トークンに分ける
-	st1 = new StringTokenizer(string1);
-	st2 = new StringTokenizer(string2);
+		//System.out.println(string1);
+		//System.out.println(string2);
 
-	// 数が異なったら失敗
-	if(st1.countTokens() != st2.countTokens()) return false;
-		
-	// 定数同士
-	for(int i = 0 ; i < st1.countTokens();){
-	    if(!tokenMatching(st1.nextToken(),st2.nextToken())){
-		// トークンが一つでもマッチングに失敗したら失敗
-		return false;
-	    }
-	}
-	
-	// 最後まで O.K. なら成功
-	return true;
-    }
+		// 同じなら成功(トークンに分割前)
+		if(string1.equals(string2)) return true;
 
-    boolean tokenMatching(String token1,String token2){
-	//System.out.println(token1+"<->"+token2);
-	if(token1.equals(token2)) return true;
-	if( var(token1) && !var(token2)) return varMatching(token1,token2);
-	if(!var(token1) &&  var(token2)) return varMatching(token2,token1);
-	return false;
-    }
+		// 各々トークンに分ける(StringTokenizer(String)でトークンに分割されたものがst1に)
+		st1 = new StringTokenizer(string1);
+		st2 = new StringTokenizer(string2);
 
-    boolean varMatching(String vartoken,String token){
-	if(vars.containsKey(vartoken)){
-	    if(token.equals(vars.get(vartoken))){
+		// 数が異なったら失敗
+		if(st1.countTokens() != st2.countTokens()) return false;
+
+		// 定数同士
+		for(int i = 0 ; i < st1.countTokens();){
+			//nextToken()でString
+		    if(!tokenMatching(st1.nextToken(),st2.nextToken())){
+			// トークンが一つでもマッチングに失敗したら失敗
+		    	return false;
+		    }
+		}
+
+		// 最後まで O.K. なら成功
 		return true;
-	    } else {
+    }
+
+    //各トークンごとにマッチングを行う
+    boolean tokenMatching(String token1,String token2){
+    	//System.out.println(token1+"<->"+token2);
+    	//どちらも変数でない
+		if(token1.equals(token2)) return true;
+		//どちらかが変数
+		if( var(token1) && !var(token2)) return varMatching(token1,token2);
+		if(!var(token1) &&  var(token2)) return varMatching(token2,token1);
 		return false;
-	    }
-	} else {
-	    vars.put(vartoken,token);
-	}
-	return true;
+    }
+
+    //変数がマッチングするか
+    boolean varMatching(String vartoken,String token){
+    	//すでに変数が束縛されているか
+		if(vars.containsKey(vartoken)){
+			//束縛されているのならマッチングしているか
+		    if(token.equals(vars.get(vartoken))){
+		    	return true;
+		    }else{
+		    	return false;
+		    }
+		}else{
+			//変数を束縛してハッシュテーブルに登録
+		    vars.put(vartoken,token);
+		}
+		return true;
     }
 
     boolean var(String str1){
-	// 先頭が ? なら変数
-	return str1.startsWith("?");
+		// 先頭が ? なら変数
+		return str1.startsWith("?");
     }
 
 }
